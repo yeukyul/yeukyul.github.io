db64fbe3775f0acff8070d340ba8ae70600c6f64
Tiffany Lee <lyeukyu@gmail.com>
2017-06-20 15:44:42 -0700

    added sidebar toggle

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 /* sidebar javascript */
 
 var settings = {
-      		toggle: "#sliiider-toggle", // the selector for the menu toggle, whatever clickable element you want to activate or deactivate the menu. A click listener will be added to this element.
+      		toggle: ".sliiider-toggle", // the selector for the menu toggle, whatever clickable element you want to activate or deactivate the menu. A click listener will be added to this element.
       		exit_selector: ".slider-exit", // the selector for an exit button in the div if needed, when the exit element is clicked the menu will deactivate, suitable for an exit element inside the nav menu or the side bar
       		animation_duration: "0.5s", //how long it takes to slide the menu
-     		place: "left", //where is the menu sliding from, possible options are (left | right | top | bottom)
+     		place: "right", //where is the menu sliding from, possible options are (left | right | top | bottom)
       		animation_curve: "cubic-bezier(0.54, 0.01, 0.57, 1.03)", //animation curve for the sliding animation
       		body_slide: true, //set it to true if you want to use the effect where the entire page slides and not just the div
      		no_scroll: false, //set to true if you want the scrolling disabled while the menu is active
