f29cbab96237b7fe276420ef10a774bf316b57fc
Tiffany Lee <lyeukyu@gmail.com>
2020-08-07 18:52:25 -0700

    Updates

---
+++ precache-manifest.d4ed1a4bc3eca3187f8c416581a8d741.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3688aee64742c2f4aeb5d8262ddb97c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba05ad6d9bc5b5c32b80",
+    "url": "/static/css/main.4c61674d.chunk.css"
+  },
+  {
+    "revision": "44bdbc21f4f40543b958",
+    "url": "/static/js/2.44d41a39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.44d41a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba05ad6d9bc5b5c32b80",
+    "url": "/static/js/main.3a07998f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "c0d6c818e5dcc71e9991dc3c8260c785",
+    "url": "/static/media/cmu_logo.c0d6c818.png"
+  },
+  {
+    "revision": "f5a41bf9051526a10e9fd7504bbc6d1b",
+    "url": "/static/media/cmute_logo.f5a41bf9.png"
+  },
+  {
+    "revision": "75e42c5ec529d84e7a0787d65aacc11e",
+    "url": "/static/media/me.75e42c5e.jpg"
+  }
+]);
